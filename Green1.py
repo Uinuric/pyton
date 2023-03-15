47ed0f72236e54a4c28419384149878290146ec4
Ринат Нурмухаметов <rinreg@inbox.ru>
2023-03-15 19:53:49 +0800

    + ввод

--- Green1.py
+++ Green1.py
@@ -1,5 +1,11 @@
-var_1 = 1
+var_num = 1
 var_str = "text"
 
-print(var_1)
+print(var_num)
 print(var_str)
+
+var_str = input("Введите строку ")
+print(var_str)
+
+var_num = int(input("Введите число "))
+print(var_num)
