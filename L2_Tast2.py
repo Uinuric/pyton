6645b94d0dc5439ad55185b131d2581e73884446
Ринат Нурмухаметов <rinreg@inbox.ru>
2023-03-18 22:30:24 +0800

    Задание 2 готово

---
+++ L2_Tast2.py
@@ -0,0 +1,12 @@
+my_list = input("Введите элементы списка: ")
+# print(my_list)
+count_el = int(len(my_list) / 2)
+end_el = int(len(my_list))
+end_list = []
+for el in range(count_el):
+    end_list.append(my_list[2 * el + 1])
+    end_list.append(my_list[2 * el])
+if (len(my_list) % 2) > 0:
+    end_list.append(my_list[end_el - 1])
+#     el = el + 1
+print(end_list)
