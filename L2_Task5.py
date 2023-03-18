44f40dfab647f31cc9a36d67931b9a3dab4496b2
Ринат Нурмухаметов <rinreg@inbox.ru>
2023-03-19 00:29:56 +0800

    +

--- L2_Task5.py
+++ L2_Task5.py
@@ -1,5 +1,5 @@
 my_var = int(input("Введите элемент рейтинга: "))
-#my_var = 9
+# my_var = 9
 my_list = [7, 5, 3, 3, 2]
 for el in range(len(my_list)):
     if my_var > my_list[el]:
--- L2_Tast2.py
+++ L2_Tast2.py
@@ -6,7 +6,7 @@ end_list = []
 for el in range(count_el):
     end_list.append(my_list[2 * el + 1])
     end_list.append(my_list[2 * el])
-if (len(my_list) % 2) > 0:
+if len(my_list) % 2 > 0:
     end_list.append(my_list[end_el - 1])
 #     el = el + 1
 print(end_list)
