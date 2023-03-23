f3e3a0e3ff49b21469c7eacd1554ee4295ce122a
green <127582212+Uinuric@users.noreply.github.com>
2023-03-23 14:29:43 +0800

    Delete Task2.py

--- Task2.py
+++
@@ -1,9 +0,0 @@
-var_input = int(input("Введите время в секундах "))
-# var_input = 5000
-var_hour = var_input // 3600
-var_min = (var_input - 3600) // 60
-var_sec = (var_input - var_hour * 3600 - var_min * 60)
-
-var_hour = var_input // 3600
-
-print(f" {var_hour}:{var_min}:{var_sec}")
